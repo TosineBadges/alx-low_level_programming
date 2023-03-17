856ee031633922f6b8621b14f97011d2d8ab29a7
TosineBadges <tosinebadges@gmail.com>
2023-03-17 12:54:43 -0700

    Single Digit Numbers Combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-	putchar(n);
-	if (n != 10)
-	putchar(',');
-	putchar(' ');
+		putchar((n % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
