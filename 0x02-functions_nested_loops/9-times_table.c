a1a94dc218c99d13137e595df8f88a9505b9b6c6
TosineBadges <tosinebadges@gmail.com>
2023-03-21 09:17:51 -0700

    9 Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,7 @@
 
 /**
  * times_table - print the 9 times table
+ * Return: None
  */
 void times_table(void)
 {
@@ -9,21 +10,26 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 0; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num + mult;
-
-			if (prod <= 9)
+			prod = (num * mult);
+			if (mult != 0)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
+			}
+			if (prod >= 10)
+			{
 				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else if (prod < 10 && mult != 0)
+			{
+				_putchar(',');
+				_putchar((prod % 10) + '0');
+			}
+			else
+				_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
