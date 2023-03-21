3d02d133aeeb51e926f50c4eaa198b5a38aef742
TosineBadges <tosinebadges@gmail.com>
2023-03-21 09:36:52 -0700

    The Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_last_digit - prints last digit
  * @n: the number
@@ -8,7 +7,7 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
-		n *= -1;
+		n = n * -1;
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
