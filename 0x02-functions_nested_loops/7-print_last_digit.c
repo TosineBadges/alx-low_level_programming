050e3f4b96dc63059e4c08cdbc64408961c90ba7
TosineBadges <tosinebadges@gmail.com>
2023-03-21 09:29:48 -0700

    The last Digit of a Number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,8 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
-
-	if (ld < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-	return (0);
+	if (n < 0)
+		n *= -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
