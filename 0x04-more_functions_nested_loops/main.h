f420f3eb12033c076da29984e06c49afe041cba7
TosineBadges <tosinebadges@gmail.com>
2023-03-23 08:50:27 -0700

    largest prime factor of the number 612852475143

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,5 +13,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
+int isPrime(long int n);
+long int biggestFactor(long int a);
+void print_number(int n);
 #endif
