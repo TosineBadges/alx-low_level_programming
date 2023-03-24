f0d51609a789b0a5f53c46ea80c08e6538ae38eb
TosineBadges <tosinebadges@gmail.com>
2023-03-23 19:32:39 -0700

    Task 1 done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,17 +6,18 @@
 
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*
-*while (i < 10)
-*{
-*		putchar(i);
-*}
-*/
-printf("Infinte loop avoided! \\o/\n");
+	printf("Infinite loop incoming :(\n");
 
-return (0);
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("Infinte loop avoided! \\o/\n");
+
+	return (0);
 }
