3d8c33e46b3f8a8bf28fe2dada88f6d1cf487875
TosineBadges <tosinebadges@gmail.com>
2023-03-29 10:56:40 -0700

    Work completed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,10 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
 #endif
