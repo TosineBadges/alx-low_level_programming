6ccccd84d5573b6368427751f75eedd013ac7f06
TosineBadges <tosinebadges@gmail.com>
2023-04-09 16:12:15 -0700

    \program that prints its name then a new line

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,6 +5,6 @@ int main(int argc, __attribute__((unused)) char **argv);
 int main(int argc, char **argv);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
-int main(int argc, char *argv[])
-int _putchar(char c)
+int main(int argc, char *argv[]);
+int _putchar(char c);
 #endif
